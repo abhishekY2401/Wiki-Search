7ffbecdaa11f75b7eb06bd7fe81d991e0a57158d
abhishekY2401 <abhiyd2401@gmail.com>
2022-01-09 19:27:10 -0800

    produces a error

---
+++ src/components/NoDefinition.js
@@ -0,0 +1,16 @@
+import "../App";
+
+function NoDefinition() {
+
+    return(
+        <div className="error">
+            <h2 className="no-def-h2">No <span>definition</span> found.
+            </h2>
+            <p className="no-def-p">
+                <span>Please</span> try to be <span>specific</span>!
+            </p>
+        </div>
+    )
+}
+
+export default NoDefinition;
